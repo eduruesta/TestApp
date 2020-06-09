c9e9647681134992f9fb7bb8968dd314d8936367
eduardo.ruesta <eduardo.ruesta@despegar.com>
2020-06-09 10:21:20 -0300

    add info in the right screen

--- app/src/main/java/com/example/testapp/adapter/BaseViewHolder.kt
+++ app/src/main/java/com/example/testapp/adapter/BaseViewHolder.kt
@@ -4,10 +4,16 @@ import android.content.Context
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.testapp.domain.RedditChildren
+import com.example.testapp.domain.RedditChildrenInformation
 
 abstract class BaseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     protected abstract fun clear()
 
-    abstract fun onBind(position: Int, topReddit: RedditChildren, context:Context)
+    abstract fun onBind(
+        position: Int,
+        topReddit: RedditChildren,
+        context: Context,
+        clickListener: (redditChildren: RedditChildrenInformation) -> Unit
+    )
 }
--- app/src/main/java/com/example/testapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/testapp/ui/MainActivity.kt
@@ -6,12 +6,19 @@ import android.os.Bundle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DefaultItemAnimator
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.example.testapp.R
 import com.example.testapp.adapter.RedditAdapter
+import com.example.testapp.domain.RedditChildren
+import com.example.testapp.domain.RedditChildrenInformation
 import com.example.testapp.domain.TopReddit
 import com.example.testapp.viewmodels.RedditViewModel
+import kotlinx.android.synthetic.main.redd_items_info.*
+import kotlinx.android.synthetic.main.redd_items_info.view.*
+import kotlinx.android.synthetic.main.redd_items_info.view.imageInfo
+import kotlinx.android.synthetic.main.redd_items_info.view.subtitleInfo
+import kotlinx.android.synthetic.main.redd_items_info.view.titleInfo
 import kotlinx.android.synthetic.main.redd_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -33,9 +40,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun prepareRecyclerView(topRedditList: TopReddit) {
-        redditAdapter = RedditAdapter(applicationContext, topRedditList.data.children)
+        redditAdapter = RedditAdapter(applicationContext, topRedditList.data.children) { item -> doClick(item)}
 
-       if (this.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+        if (this.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
             redditRecyclerView.layoutManager = (LinearLayoutManager(this))
         } else {
             redditRecyclerView.layoutManager = (LinearLayoutManager(this))
@@ -44,4 +51,15 @@ class MainActivity : AppCompatActivity() {
         redditRecyclerView.adapter = redditAdapter
         redditAdapter.notifyDataSetChanged()
     }
+
+    private fun doClick(item: RedditChildrenInformation) {
+        titleInfo.text = item.author
+        Glide.with(applicationContext)
+            .load(item.thumbnail)
+            .into(imageInfo)
+        subtitleInfo.text = item.title
+    }
+
 }
+
+
